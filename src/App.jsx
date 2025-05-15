213b8451916410bcefe9541c16d1e0cc3f9adf10
unknown <jubintajj@gmail.com>
2025-05-15 13:01:14 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ import { ToastContainer } from 'react-toastify'
 import "react-toastify/dist/ReactToastify.css";
 import Wishlist from './pages/Wishlist'
 import AdminOrder from './admin/adminorder'
-import Home from './pages/home'
+import Home from './pages/Home'
 
 
 function App() {
